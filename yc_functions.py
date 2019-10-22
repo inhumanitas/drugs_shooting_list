d835be5d0bc9c61e419e581f7cb462b672473c8b
Danis Valiullin <v.danis08@gmail.com>
2019-10-22 22:12:38 +0300

    Notify admin about users interaction

--- yc_functions.py
+++ yc_functions.py
@@ -1,7 +1,7 @@
 from telegram import ParseMode
 
-from drugs_shooting_list.bot import bot
-from drugs_shooting_list.utils import get_drug_info, to_tg_update, DATA
+from drugs_shooting_list.bot import bot, notify_admin
+from drugs_shooting_list.utils import get_drug_info, to_tg_update
 
 
 def echo_handler(update, context):
@@ -23,13 +23,15 @@ def echo_handler(update, context):
 
 @to_tg_update(bot)
 def message_handler(update, context):
-
+    result = get_drug_info(update.message.text)
     bot.send_message(
         update.message.chat.id,
-        get_drug_info(update.message.text),
+        result,
         disable_web_page_preview=True,
         parse_mode=ParseMode.HTML,
     )
+    user = update.message.from_user
+    notify_admin(f'{update.message.chat.id} @{user.name} {user.username} {user.first_name} {user.last_name} {user.username}: {update.message.text}\n{result}')
 
     return {
         'statusCode': 200,
