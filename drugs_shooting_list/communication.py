c5c014b8eb7022fa0c979eaac23fb6e2a3658317
inhumanitas <v.danis08@gmail.com>
2019-11-03 20:49:22 +0000

    Add inline quering

--- drugs_shooting_list/communication.py
+++ drugs_shooting_list/communication.py
@@ -1,43 +1,67 @@
-import difflib
+from uuid import uuid4
 
-from telegram import ParseMode
+from telegram import ParseMode, InlineQueryResultArticle, \
+    InputTextMessageContent
 
 from drugs_shooting_list.bot import bot, notify_admin
 from drugs_shooting_list.settings import ADMIN_UID
-from drugs_shooting_list.utils import to_tg_update, get_drug_info, DATA
+from drugs_shooting_list.utils import to_tg_update, get_drug_info, predict_key, \
+    DATA
 
 greeting_msg = 'Добро пожаловать'
 
 
-def predict_key(key):
-    return difflib.get_close_matches(key, DATA.keys, 1, 0)
+def inline_search(update):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(), title=x,
+            input_message_content=InputTextMessageContent(
+                get_drug_info(x), parse_mode=ParseMode.HTML))
+        for x in DATA.get_subkeys(query.lower())
+    ]
+    return update.inline_query.answer(results)
 
 
 @to_tg_update(bot)
 def process_message(update):
+    if update.inline_query:
+        print('inline_search ' + str(update.inline_query))
+        return inline_search(update)
+
+    if not update.message:
+        return
 
     text = update.message.text
     user_chat_id = update.message.chat.id
 
     if text == '/start':
+        print('Greeting new user ' + str(user_chat_id))
         bot.send_message(
             user_chat_id,
             greeting_msg,
         )
         result = f'New user {user_chat_id}'
+
     else:
-        keys = predict_key(text)
-
-        text = keys[0] if keys else text
-
         result = get_drug_info(text)
-
-        bot.send_message(
-            user_chat_id,
-            result,
-            disable_web_page_preview=True,
-            parse_mode=ParseMode.HTML,
-        )
+        if result:
+            bot.send_message(
+                user_chat_id,
+                result,
+                disable_web_page_preview=True,
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            keys = predict_key(text, words=5)
+            result = 'Не найдено. Попробуйте варианты: ' + ', '.join(keys)
+            bot.send_message(
+                user_chat_id,
+                result,
+                disable_web_page_preview=True,
+                parse_mode=ParseMode.HTML,
+            )
 
     if user_chat_id != ADMIN_UID:
         user = update.message.from_user
--- drugs_shooting_list/utils.py
+++ drugs_shooting_list/utils.py
@@ -1,29 +1,35 @@
+import difflib
 import json
 import os
 
 from functools import wraps
 
-from drugs_shooting_list.settings import DATA_FILE_PATH
+from drugs_shooting_list.settings import DATA_FILE_PATH, INLINE_QUERY_LEN, \
+    INLINE_QUERY_COUNT
 
 
 class Data:
     _data = None
 
     @property
+    def data(self):
+        if self._data is None:
+            self.load()
+        return self._data
+
+    @property
     def keys(self):
-        return self._data.keys() if self._data else []
+        return self.data.keys() if self.data else []
 
     def load(self, json_path=DATA_FILE_PATH):
         self._data = json.load(open(os.path.expandvars(json_path)))
 
-    def get(self, key, not_found_message, processed_keys=None):
-        if not self._data:
-            self.load()
-        result = not_found_message
+    def get(self, key, default_value, processed_keys=None):
+        result = default_value
         key = key.lower()
         processed_keys = processed_keys or [key]
-        if key in self._data:
-            keys, value = self._data.get(key)
+        if key in self.data:
+            keys, value = self.data.get(key)
             if value:
                 result = value
             elif keys:
@@ -38,12 +44,28 @@ class Data:
                         break
         return result
 
+    def get_subkeys(self, substr):
+        """Find the keys that are starts with `substr`
+        :param substr: key substring
+        :return first N values that starts with `substr` or [] if not found
+        """
+        result = []
+        if len(substr) >= INLINE_QUERY_LEN:
+            for key in self.keys:
+                if len(result) >= INLINE_QUERY_COUNT:
+                    break
+
+                if key.startswith(substr):
+                    result.append(key)
+        return result
+
 
 DATA = Data()
 
 
 def to_tg_update(bot):
     from telegram import Update
+
     def wrapped(fn):
         @wraps(fn)
         def inner(event, *args, **kwargs):
@@ -53,11 +75,15 @@ def to_tg_update(bot):
     return wrapped
 
 
-def get_drug_info(drug: str, not_found_message: str = 'Не найдено'):
+def get_drug_info(drug: str, default_value=None):
     """Retrieve info by drug name
 
     :param drug: drug name
-    :param not_found_message: return message for not found rows
+    :param default_value: value for not found rows
     :return: info about drug
     """
-    return DATA.get(drug, not_found_message)
+    return DATA.get(drug, default_value)
+
+
+def predict_key(key, words=1):
+    return difflib.get_close_matches(key, DATA.keys, words, 0)
