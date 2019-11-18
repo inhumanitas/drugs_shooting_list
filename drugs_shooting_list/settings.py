9a27b00bcb397fd4e3eb3424b77a10978f756e8d
inhumanitas <1991249+inhumanitas@users.noreply.github.com>
2019-11-19 00:28:41 +0300

    Create .runtests.yml

--- drugs_shooting_list/settings.py
+++ drugs_shooting_list/settings.py
@@ -11,7 +11,7 @@ if os.path.exists(local_settings_file):
 
 TOKEN = local_settings.get('TOKEN')
 PROXY = local_settings.get('PROXY')
-ADMIN_UID = int(local_settings.get('ADMIN_UID'))
+ADMIN_UID = int(local_settings.get('ADMIN_UID', 0))
 INLINE_QUERY_LEN = int(local_settings.get('INLINE_QUERY_LEN', 3))
 INLINE_QUERY_COUNT = int(local_settings.get('INLINE_QUERY_COUNT', 6))
 DATA_FILE_PATH = local_settings.get('DATA_FILE_PATH') or 'results.json'
